a66ad1b025e86806a26b7748ba8f11615b135b13
Franco Fontana <francomartinfontana@gmail.com>
2022-08-22 16:48:36 -0300

    new structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { SignInComponent } from './routes/sign-in/sign-in.component';
+import { BoardComponent } from './routes/board/board.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    BoardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
